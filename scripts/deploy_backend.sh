874e6b47f24aad9a22e7f3f531207b4080fc40d3
Ubuntu <ubuntu@ip-172-31-91-133.ec2.internal>
2023-11-24 13:11:11 +0000

    Scripts creados

---
+++ scripts/deploy_backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+apt update
+
+#apt upgrade -y
+
+source .env
+
+systemctl restart mysql
+
+mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "FLUSH PRIVILEGES"
