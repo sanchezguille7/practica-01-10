1fa7957e8edfed7c3ac6fcfe4fb0eec9f4dd4fdd
Ubuntu <ubuntu@ip-172-31-81-14.ec2.internal>
2023-11-27 13:46:12 +0000

    terminado, solo falta readme

--- scripts/install_load_balancer.sh
+++ scripts/install_load_balancer.sh
@@ -8,15 +8,25 @@ apt update
 
 apt install apache2 -y
 
-cp ../load-balancer.conf /etc/apache2/sites-available
+cp /home/ubuntu/practica-01-10/conf/load-balancer.conf /etc/apache2/sites-available
 
-sed -i "s/$IP_HTTP_SERVER1/" /etc/apache/sites-available/load-balancer.conf
-sed -i "s/$IP_HTTP_SERVER2/" /etc/apache/sites-available/load-balancer.conf
+#sed -i "s/$IP_HTTP_SERVER1/" /etc/apache/sites-available/load-balancer.conf
+#sed -i "s/$IP_HTTP_SERVER2/" /etc/apache/sites-available/load-balancer.conf
 
 a2enmod proxy
 
 a2enmod proxy_http
 
 a2enmod proxy_balancer
+
+a2enmod lbmethod_byrequests
+
+systemctl restart apache2
+
+a2ensite load-balancer.conf 
+
+a2dissite 000-default.conf 
+
+apache2ctl -S
 
 systemctl restart apache2
