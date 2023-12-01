027f5f3ace04c4433b716946d684609d0be7b88c
Ubuntu <ubuntu@ip-172-31-80-68.ec2.internal>
2023-12-01 11:35:00 +0000

    Fin, solo falta readme

--- scripts/install_lamp_frontend.sh
+++ scripts/install_lamp_frontend.sh
@@ -13,5 +13,3 @@ cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
 sudo apt install php libapache2-mod-php php-mysql -y
 
 systemctl restart apache2
-
-chown -R www-data:www-data /var/www/html
